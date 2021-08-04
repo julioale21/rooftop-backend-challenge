62336f07e97500c6c314a6edc0fe2f2c3652301f
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-08-04 11:44:06 -0300

    chore: added coupons routes

--- src/routes/coupons.routes.ts
+++ src/routes/coupons.routes.ts
@@ -1,5 +1,12 @@
 import { Router } from "express";
+import { createCoupon, getCoupons } from "../controllers/coupons.controller";
 
 const router = Router();
+
+router.get("/coupons", getCoupons);
+// router.get("/coupons/:id");
+router.post("/coupons", createCoupon);
+// router.put("/coupons");
+// router.delete("/coupons/:id");
 
 export default router;
