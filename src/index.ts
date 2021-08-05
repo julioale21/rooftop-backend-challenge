313fd006dd2528212bd57e0d3b4f97ed464effd3
Julio Romero <julioale21@gmail.com>
2021-08-05 14:30:04 -0300

    chore: added dotenv config

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@ import "reflect-metadata";
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-
+import * as dotenv from "dotenv";
 import { createConnection } from "typeorm";
-
 import couponsRoutes from "./routes/coupons.routes";
 import storesRoutes from "./routes/stores.routes";
+
+dotenv.config({ path: __dirname + "/.env" });
 
 const app = express();
 createConnection();
