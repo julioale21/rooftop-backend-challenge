8a606e23b7e14749f6fab98ffa6d8ce8244946f9
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-08-05 12:48:34 -0300

    chore: updated coupons controller, adde method to find a coupon by its code and customer_email

--- src/controllers/coupons.controller.ts
+++ src/controllers/coupons.controller.ts
@@ -1,14 +1,18 @@
 import { Request, Response } from "express";
-import { getRepository } from "typeorm";
+import CouponsService from "../services/coupons.service";
+// import { getRepository } from "typeorm";
 import { Coupon } from "../entities/Coupon";
 
-export const getCoupons = async (req: Request, res: Response): Promise<Response> => {
-  const coupons = await getRepository(Coupon).find();
-  return res.json(coupons);
-};
+export const getCoupon = async (req: Request, res: Response): Promise<Response> => {
+  if (!req.query.email) return res.send("falta el email");
+  if (!req.query.code) return res.send("falta el codigo");
 
-export const createCoupon = async (req: Request, res: Response): Promise<Response> => {
-  const newCoupon = await getRepository(Coupon).create(req.body);
-  const response = getRepository(Coupon).save(newCoupon);
-  return res.json(response);
+  const coupon: Coupon = await CouponsService.getCoupon(
+    String(req.query.email),
+    String(req.query.code),
+  );
+
+  if (!coupon) return res.status(404).send({ message: "Not coupon found" });
+
+  return res.status(200).json(coupon);
 };
