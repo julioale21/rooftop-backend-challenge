c15862b57fa857380516f3293f02ae146e44e458
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-08-06 10:47:16 -0300

    chore: added create and findByCode

--- src/services/coupons.service.ts
+++ src/services/coupons.service.ts
@@ -2,11 +2,27 @@ import { getRepository } from "typeorm";
 import { Coupon } from "../entities/Coupon";
 
 export default class CouponsService {
-  public static getCoupon = async (email?: string, code?: string): Promise<any> => {
+  public static getByEmailAndCode = async (email?: string, code?: string): Promise<any> => {
     const coupon = await getRepository(Coupon).findOne({
       where: { customer_email: email, code: code },
     });
 
     return coupon;
+  };
+
+  public static findByCode = async (code: string): Promise<any> => {
+    const coupon = await getRepository(Coupon).findOne({ where: { code: code } });
+    return coupon;
+  };
+
+  public static create = async (code: string): Promise<Coupon> => {
+    const newCoupon = await getRepository(Coupon).create();
+    const today = new Date();
+    var newDate = new Date(today.setMonth(today.getMonth() + 1));
+    newCoupon.code = code;
+    newCoupon.expires_at = newDate;
+
+    await getRepository(Coupon).save(newCoupon);
+    return newCoupon;
   };
 }
