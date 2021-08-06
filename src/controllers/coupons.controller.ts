bc6c58fcefeea631e5c82e79441d640379908987
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-08-06 10:47:43 -0300

    chore: added create coupon on controller and validations

--- src/controllers/coupons.controller.ts
+++ src/controllers/coupons.controller.ts
@@ -3,11 +3,13 @@ import CouponsService from "../services/coupons.service";
 // import { getRepository } from "typeorm";
 import { Coupon } from "../entities/Coupon";
 
-export const getCoupon = async (req: Request, res: Response): Promise<Response> => {
-  if (!req.query.email) return res.send("falta el email");
-  if (!req.query.code) return res.send("falta el codigo");
+const codeRegExp = new RegExp("^[A-Za-z0-9]{8}$");
 
-  const coupon: Coupon = await CouponsService.getCoupon(
+export const getCoupon = async (req: Request, res: Response): Promise<Response> => {
+  if (!req.query.email) return res.status(400).send({ message: "You must provide an email." });
+  if (!req.query.code) return res.status(400).send({ message: "You must provide coupon code" });
+
+  const coupon: Coupon = await CouponsService.getByEmailAndCode(
     String(req.query.email),
     String(req.query.code),
   );
@@ -15,4 +17,24 @@ export const getCoupon = async (req: Request, res: Response): Promise<Response> 
   if (!coupon) return res.status(404).send({ message: "Not coupon found" });
 
   return res.status(200).json(coupon);
+};
+
+export const createCoupon = async (req: Request, res: Response): Promise<Response> => {
+  const code = req.body.code;
+  const match = codeRegExp.test(code);
+
+  if (!match)
+    return res.status(400).send({
+      message: "Missing or wrong code. Code must be 8 characters and contains chars and numbers",
+    });
+
+  const existedCoupon = await CouponsService.findByCode(code);
+
+  if (existedCoupon) return res.status(409).send({ message: "Code already in use" });
+
+  const newCoupon = await CouponsService.create(code);
+
+  if (!newCoupon) return res.status(422).send({ message: "Coupon could not be created" });
+
+  return res.status(201).send({ message: "Coupon successfully created" });
 };
