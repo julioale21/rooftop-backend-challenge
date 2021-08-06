74488537f96cd4eb48a7dbd8a51f91fd41f63190
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-08-06 12:31:12 -0300

    chore: added delete route

--- src/routes/coupons.routes.ts
+++ src/routes/coupons.routes.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
-import { createCoupon, getCoupon, updateCoupon } from "../controllers/coupons.controller";
+import {
+  createCoupon,
+  deleteCoupon,
+  getCoupon,
+  updateCoupon,
+} from "../controllers/coupons.controller";
 
 const router = Router();
 
 router.get("/coupons", getCoupon);
 router.post("/coupons", createCoupon);
 router.patch("/coupons/:id", updateCoupon);
-// router.delete("/coupons/:id");
+router.delete("/coupons/:id", deleteCoupon);
 
 export default router;
