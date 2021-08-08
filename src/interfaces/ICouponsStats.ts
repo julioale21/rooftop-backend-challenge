cf25d9b969adb8a6f9c6df36aa12570a4e175128
Julio Romero <julioale21@gmail.com>
2021-08-08 01:34:07 -0300

    chore: added ICouponsStats interface

---
+++ src/interfaces/ICouponsStats.ts
@@ -0,0 +1,7 @@
+export default interface ICouponsStats {
+  totalExistingCoupons: number;
+  totalAssignedCoupons: number;
+  totalUnassigned: number;
+  totalCreatedByDay: any;
+  totalAssignedByDay: any;
+}
