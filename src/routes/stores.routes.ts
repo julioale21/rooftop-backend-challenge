8c3f36c3a9bf9fdc1b7c7c6a896ed0938bc243f7
Julio Romero <julioale21@gmail.com>
2021-08-07 21:03:01 -0300

    chore: added post and delete routes

--- src/routes/stores.routes.ts
+++ src/routes/stores.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { getStores } from "../controllers/store.controllers";
+import { createStore, deleteStore, getStores } from "../controllers/store.controllers";
 
 const router = Router();
 
 router.get("/stores", getStores);
+router.post("/stores", createStore);
+router.delete("/stores/:id", deleteStore);
 
 export default router;
