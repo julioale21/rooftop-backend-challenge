8e1705d6db30adf71e8ba8d8653f120d3c3213f6
Julio Romero <julioale21@gmail.com>
2021-08-08 01:33:40 -0300

    chore: added stats controller

--- src/controllers/stasts.controller.ts
+++ src/controllers/stasts.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
+import StatsService from "../services/stats.service";
 
 export const getStats = async (req: Request, res: Response): Promise<Response> => {
-  return res.send({ message: "enviado" });
+  const response = await StatsService.getCouponsStats();
+  return res.status(200).json(response);
 };
