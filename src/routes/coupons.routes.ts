ddb24ffd0f2402b7239ed7f12057ce251606f46f
Julio Romero <julioale21@gmail.com>
2021-08-07 21:03:22 -0300

    chore: added get by id route

--- src/routes/coupons.routes.ts
+++ src/routes/coupons.routes.ts
@@ -3,12 +3,14 @@ import {
   createCoupon,
   deleteCoupon,
   getCoupon,
+  getCouponById,
   updateCoupon,
 } from "../controllers/coupons.controller";
 
 const router = Router();
 
 router.get("/coupons", getCoupon);
+router.get("/coupons/:id", getCouponById);
 router.post("/coupons", createCoupon);
 router.patch("/coupons/:id", updateCoupon);
 router.delete("/coupons/:id", deleteCoupon);
